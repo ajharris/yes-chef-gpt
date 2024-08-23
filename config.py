d29495de6b5be8b31ec1c41a7927ea059bf39fea
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-08-23 18:04:05 -0400

    cleanup

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///chefgpt.db').replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
