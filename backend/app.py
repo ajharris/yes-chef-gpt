b3382a6ccc950c402d9789263367d4fdbf0c78c3
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-08-23 22:54:34 -0400

    still cleaning up

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
-from backend.config import Config
-from backend.models import db
-from backend.routes import main_blueprint
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Load environment variables from .env file.
+
+from flask import Flask
+from config import Config
+from models import db
+from routes import main_blueprint
 
 app = Flask(__name__, static_folder='../frontend/build', template_folder='../frontend/build')
-
 
 app.config.from_object(Config)
 
@@ -15,4 +18,3 @@ app.register_blueprint(main_blueprint)
 
 if __name__ == "__main__":
     app.run(debug=True)
-                                                                            
