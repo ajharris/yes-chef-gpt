29e7c5b7f80336881101f3524ff46851ab6792b9
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-08-23 23:06:51 -0400

    hope this ends the cleanup

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,14 @@
 from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load environment variables from .env file.
+load_dotenv()  # This will load the .env file at the application startup
 
 from flask import Flask
-from .config import Config
-from .models import db
-from .routes import main_blueprint
+from config import Config
+from models import db
+from routes import main_blueprint
 
 app = Flask(__name__, static_folder='../frontend/build', template_folder='../frontend/build')
-
 app.config.from_object(Config)
-
 db.init_app(app)
-
 app.register_blueprint(main_blueprint)
 
 if __name__ == "__main__":
