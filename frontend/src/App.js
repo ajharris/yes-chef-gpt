b24dacd6fb8e033712b2b5f41cb5f648c7d9dc79
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-08-24 17:03:49 -0400

    still cleaning up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Welcome to ChefGPT!</h1>
-      <p>This is your React App working with Flask.</p>
+    <div className="App">
+      <button className="btn btn-primary">Click me!</button>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import { AppRegistry } from 'react-native';
 import App from './App'; // Make sure this is pointing to App.js
 import { createRoot } from 'react-dom/client';
 import { Platform } from 'react-native';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 if (Platform.OS === 'web') {
   const rootElement = document.getElementById('root');
